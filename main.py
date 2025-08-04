c587566bd1ae27f22d031ade4a5dce2d1486fe11
dreyestaha <dreyest@gmail.com>
2025-08-04 19:56:39 -0400

    finished project with CLI selection of the book

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import count_words, count_chars, sort_characters, display_stats
+import sys
 
 def get_book_text(file_path):
     with open(file_path) as f:
@@ -6,13 +7,17 @@ def get_book_text(file_path):
         return file_contents
 
 def main():
-    book_path = "books/frankenstein.txt"
-    book = get_book_text(book_path)
-    word_count = count_words(book)
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        book_path = sys.argv[1]
+        book = get_book_text(book_path)
+        word_count = count_words(book)
 
-    character_count = count_chars(book)
-    character_list = sort_characters(character_count)
-    display_stats(book_path, word_count, character_list)
+        character_count = count_chars(book)
+        character_list = sort_characters(character_count)
+        display_stats(book_path, word_count, character_list)
 
 
 
